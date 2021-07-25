cf420c657b33fba6d985df574379f05631cdda70
Gilbert Ramirez <contemporarygil@gmail.com>
2021-07-25 16:24:28 -0700

    Add second rest path, get gameview of board

--- server/server.js
+++ server/server.js
@@ -17,6 +17,15 @@ app.get('/games/tictactoe',  (req, res) => {
     res.json(gameBoard);  
   });
 
+app.post('/games/tictactoe/ai', (req, res) => {
+    res.status(200).send({});
+  });
+
+  //error handling
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+});
+
 
 const port = 8080;
 
--- src/App.js
+++ src/App.js
@@ -5,14 +5,49 @@ import './App.css';
 function App() {
   const serverUrl = "http://localhost:8080/games/tictactoe";
 
+  const [gameBoard, setGameBoard] = useState(null);
+
   const fetchBoard = () => {
     axios.get(serverUrl)
     .then((resp) =>{
-      console.log(resp.data);
+      setGameBoard(gameView(resp.data))
     })
     .catch((error) => {
       console.log(error)
     })
+  }
+
+  const jsonData = JSON.stringify([]);
+
+  const handleClick = () => {
+    axios.post(`${serverUrl}/ai`, jsonData, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((resp) => {
+
+      console.log(resp.data)
+      } 
+    )
+    
+  }
+
+  const gameView = (gameBoard) => {
+    const finalView = [];
+  
+    gameBoard.forEach((row, rowIndex) => {
+      const newRow  = [];
+  
+      row.forEach((symbols, rowPlace) =>{ 
+       
+          newRow.push(<button value={`${rowIndex}, ${rowPlace}`} onClick={handleClick} className={['symbols', symbols ? symbols : ''].join(" ")}>{symbols}</button>)
+      });
+  
+      finalView.push(<div className={"rowWrapper"}> {newRow} </div>);
+    });
+    return [<div className="gridWrapper">{finalView}</div>];
   }
 
   useEffect(fetchBoard, []);
@@ -21,6 +56,7 @@ function App() {
   return (
     <div className="App">
      hello
+     {gameBoard}
     </div>
   );
 }
